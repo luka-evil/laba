67ff216b0843fd0445926dfb0e86edcd5331e2a5
dep-ghost <80980744+luka-evil@users.noreply.github.com>
2023-10-15 14:37:04 +0300

    Add files via upload

--- laba/Program.cs
+++ laba/Program.cs
@@ -5,43 +5,88 @@ using project;
 
 using (ApplicationContext db = new())
 {
-    User tom = new User { Name = "Tom", Age = 33 };
-    User alice = new User { Name = "Alice", Age = 26 };
-    User Nikita = new User { Name = "Nikita", Age = 29 };
-    User Sasha = new User { Name = "Sasha", Age = 90 };
+    Human Nikita = new Human { Name = "Niklta", Age = 13 };
+    Human 卄凵长凵丅卂 = new Human { Name = "卄凵长凵丅卂", Age = 17 };
+    Human 匚卂尺 = new Human { Name = "匚卂尺", Age = 1000 };
+    Human TomasShelbi = new Human { Name = "丅口从卂丂 丂卄乇乚乃工", Age = -7 };
 
-    db.Users.Add(Sasha);
-    db.Users.Add(Nikita);
-    db.Users.Add(tom);
-    db.Users.Add(alice);
+    CarShop IliaBudanov = new CarShop { Name = "凵人乚牙 万丫丑卂卄口乃", Quantity = 1000 };
+
+    Car Porshe = new Car { Name = "亡丹乇几", Release = 2001, Shop = IliaBudanov };
+    Car Bmw = new Car { Name = "匚卂丫乇冂冂乇", Release = 2011, Shop = IliaBudanov };
+    Car Audi = new Car { Name = "R8", Release = 2021, Shop = IliaBudanov };
+    Car Mazda = new Car { Name = "RX8", Release = 2111, Shop = IliaBudanov };
+
+    db.CarShops.Add(IliaBudanov);
+
+    db.Humans.Add(Nikita);
+    db.Humans.Add(卄凵长凵丅卂);
+    db.Humans.Add(匚卂尺);
+    db.Humans.Add(TomasShelbi);
+
+    db.Cars.Add(Porshe);
+    db.Cars.Add(Bmw);
+    db.Cars.Add(Audi);
+    db.Cars.Add(Mazda);
+
     db.SaveChanges();
     Console.WriteLine("Объекты успешно сохранены");
 
-    var users = db.Users.ToList();
+ 
     Console.WriteLine("Список объектов:");
-    foreach (User u in users)
+    foreach ( Human u in db.Humans.ToArray() )
     {
-        Console.WriteLine($".{u.Name} - {u.Age}");
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
     }
+
+    foreach (Car u in db.Cars.ToArray())
+    {
+        Console.WriteLine($"{u.Name}.{u.Release} - {u.ShopId} - {u.Id}");
+    }
+
+    foreach (CarShop u in db.CarShops.ToArray())
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Quantity}");
+    }
+
 }
 
 namespace project
 {
-    public class User
+    public class Human
     {
         public int Id { get; set; }
         public string? Name { get; set; }
         public int Age { get; set; }
     }
 
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Release { get; set; }
+        public int ShopId { get; set; }
+        public CarShop Shop { get; set; }
+    }
+
+    public class CarShop
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+        public List<Car> ArrayCars { get; set; }
+    }
+
     public class ApplicationContext : DbContext
     {
-        public DbSet<User> Users => Set<User>();
+        public DbSet<Human> Humans => Set<Human>();
+        public DbSet<Car> Cars => Set<Car>();
+        public DbSet<CarShop> CarShops => Set<CarShop>();
         public ApplicationContext() => Database.EnsureCreated();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=dbLaba;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=dbAA;Trusted_Connection=True;");
         }
     }
 
